523ef3a8744503e9fb207b30ede9e5b97ca1ae93
akhilasajeevan <151014695+akhilasajeevan@users.noreply.github.com>
2024-11-05 12:50:02 +0530

    Add Tailwind CSS and PostCSS configuration for styling setup

--- src/Components/Input/Input.jsx
+++ src/Components/Input/Input.jsx
@@ -14,7 +14,7 @@ const handleAddToTask=(e)=>{
     
       
         <form className="flex flex-row items-center gap-3">
-          <input  className="border rounded-lg  py-1.5 px-2.5 text-lg"    type="text" placeholder="Add a task" value={input} onChange={(e)=>setInput(e.target.value)}/>
+          <input  className="border border-gray-400 rounded-lg  py-1.5 px-2.5 text-lg"    type="text" placeholder="Add a task" value={input} onChange={(e)=>setInput(e.target.value)}/>
           <button  className="bg-violet-400 text-white py-2 px-3.5 rounded-lg font-semibold hover:opacity-70"
           onClick={handleAddToTask}>Add</button>
         </form>
@@ -24,3 +24,6 @@ const handleAddToTask=(e)=>{
 }
 
 export default Input;
+
+
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,10 @@
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",  // include jsx/tsx if you're using them
+    "./public/index.html"           // include index.html if it's part of the DOM structure
+  ],
   theme: {
     extend: {},
   },
